e53529040b85a5bd5ef6de3b6ea2b25196c5e76f
tuanvision <tuannm0312@gmail.com>
2020-10-13 17:48:19 +0700

    blank project

---
+++ utils/DataCloudLoader.py
@@ -0,0 +1,14 @@
+import os
+
+
+class DataCloudLoader():
+    def __init__(self):
+        pass
+    
+    def load(self):
+        print("Loading DATA Cloud")
+        pass
+
+data = DataCloudLoader()
+data.load()
+
